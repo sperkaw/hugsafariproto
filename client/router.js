657f0f330ada581ff0f547f7c39f618f02c06538
jaimiederijk <j.r.derijk@quicknet.nl>
2016-01-02 17:06:05 +0100

    new imgage shit

--- client/router.js
+++ client/router.js
@@ -3,7 +3,18 @@ Router.configure({
   	loadingTemplate: 'loading'
 });
 
+Router.route('/teamAanmelden',{
+ waitOn: function () {
+ return Meteor.subscribe('images')
+ },
+ action: function () {
+ if (this.ready())
+ this.render('teamAanmelden');
+ else
+ this.render('Loading');
+ }
+});
 
 Router.route('/', {name: 'start'});
-Router.route('/team', {name: 'teamAanmelden'});
+//Router.route('/team', {name: 'teamAanmelden'});
 Router.route('/join', {name: 'teamAansluiten'});
--- client/templates/teamaanmelden.js
+++ client/templates/teamaanmelden.js
@@ -8,3 +8,22 @@ Template.teamAanmelden.helpers({
     // callback: (error, photo) ->
     //    # Do what you want with the photo.  Save it?
 })
+
+Template.teamAanmelden.events({
+   'change .myFileInput': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+          if (err){
+             // handle error
+          } else {
+             // handle success depending what you need to do
+            var userId = Meteor.userId();
+            var imagesURL = {
+              "profile.image": "/cfs/files/images/" + fileObj._id
+            };
+            Meteor.users.update(userId, {$set: imagesURL});
+          }
+        });
+     });
+   }
+});
